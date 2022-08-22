ae1854ca07c21aa68b8577878bdf34229c13de1f
SergeyNikAl <cergei_nikulin@mail.ru>
2022-08-22 14:00:39 +0300

    first test commit

---
+++ backend/recipes/management/commands/csv_manager.py
@@ -0,0 +1,22 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        data_path = settings.BASE_DIR
+        with open(
+            f'{data_path}/data/ingredients.csv',
+            'r',
+            encoding='utf-8'
+        ) as file:
+            reader = csv.DictReader(file)
+            Ingredient.objects.bulk_create(
+                Ingredient(**data) for data in reader
+            )
+        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
