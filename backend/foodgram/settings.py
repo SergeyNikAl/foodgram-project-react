100ed6ab7a5140830f0b7cba2a215093fb6b7442
SergeyNikAl <cergei_nikulin@mail.ru>
2022-08-25 19:58:02 +0300

    first deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -82,8 +82,7 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE',
-                            default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
