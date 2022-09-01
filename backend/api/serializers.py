36d9175b79e09e7b4afdea259fa656ac6e418198
SergeyNikAl <cergei_nikulin@mail.ru>
2022-09-01 14:43:45 +0300

    fix recipes and following_list

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,3 @@
-import re
-
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
