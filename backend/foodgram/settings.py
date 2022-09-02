4eaeac669c892df5dd6524a7512e16bdc0a4c615
SergeyNikAl <cergei_nikulin@mail.ru>
2022-09-02 14:27:25 +0300

    requirements

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -17,8 +17,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'adminlte3',
-    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
